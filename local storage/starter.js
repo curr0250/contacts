a16f7c94f1846edb34efe52566dce8bbc7198138
jojcurr <curr0250@algonquinlive.com>
2017-12-19 13:01:52 -0500

    update of code
    update of code

---
+++ local storage/starter.js
@@ -0,0 +1,5 @@
+let contactStarter = [
+    {"fullname":"Harry Potter","phone":"613-123-2345","email":"mr.potter@hogwarts.com"},
+    {"fullname":"Hermione Granger","phone":"613-123-8267","email":"mrs.potter@hogwarts.com"},
+    {"fullname":"Ron Weasley","phone":"613-123-5938","email":"big.red@hogwarts.com"}
+];
